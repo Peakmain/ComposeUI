755521b653bbd8a2f4697fc0a752b506a4dcb37f
成东(周成东) <chengdong.zhou@yaduo.com>
2025-11-05 10:37:32 +0800

    [UI组件封装]ImagePainter支持动态设置解码宽高、绘制过滤质量

--- library/src/main/java/com/peakmain/compose/utils/ImagePainterUtils.kt
+++ library/src/main/java/com/peakmain/compose/utils/ImagePainterUtils.kt
@@ -5,12 +5,15 @@ import android.graphics.Insets.add
 import android.os.Build.VERSION.SDK_INT
 import android.text.TextUtils
 import androidx.annotation.DrawableRes
+import androidx.annotation.Px
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.FilterQuality
+import androidx.compose.ui.graphics.drawscope.DrawScope.Companion.DefaultFilterQuality
 import androidx.compose.ui.platform.LocalContext
 import coil.ImageLoader
 import coil.compose.AsyncImagePainter
@@ -33,6 +36,20 @@ object ImagePainterUtils {
      * @param imageUrl 图片的 URL，如果为空则显示占位图。
      * @param errorDrawableResId 图片加载失败时显示的 Drawable 资源 ID，默认为 R.drawable.icon_loading。
      * @param placeDrawableResId 图片加载过程中显示的占位图 Drawable 资源 ID，默认为 R.drawable.icon_loading。
+     * @param targetWidth   目标解码宽度（像素）。
+     *                      - 当不为 `0` 时，会在解码阶段下采样图片，减少内存占用。
+     *                      - 建议设置为与容器宽度相近的像素值，避免解码原图造成高内存开销。
+     * @param targetHeight  目标解码高度（像素）。
+     *                       - 与 [targetWidth] 配合使用，控制图片的最终解码尺寸。
+     *                       - 如果容器为固定比例（如 `ContentScale.Crop`），建议解码尺寸略大于显示区域，防止缩放模糊。
+     *
+     * @param filterQuality     绘制过滤质量（影响缩放清晰度与性能）。
+     *                          - 默认值：`DefaultFilterQuality`（即 [FilterQuality.Low]）
+     *                          - 可选值：
+     *                               - `FilterQuality.None`：最快速，但缩放后可能出现锯齿。
+     *                               - `FilterQuality.Low`：平衡性能与画质。
+     *                               - `FilterQuality.Medium`：更平滑的缩放效果，性能中等。
+     *                               - `FilterQuality.High`：最高画质，适合缩放后仍需高清展示的场景（代价是性能略低）。
      * @return 返回一个 AsyncImagePainter 对象。
      */
     @Composable
@@ -40,24 +57,32 @@ object ImagePainterUtils {
         imageUrl: String?,
         @DrawableRes errorDrawableResId: Int = R.drawable.icon_loading,
         @DrawableRes placeDrawableResId: Int = R.drawable.icon_loading,
-        imageLoader: ImageLoader = getImageLoader(LocalContext.current)
+        @Px targetWidth: Int = 0,
+        @Px targetHeight: Int = 0,
+        filterQuality: FilterQuality = DefaultFilterQuality,
+        imageLoader: ImageLoader = getImageLoader(LocalContext.current),
     ): AsyncImagePainter {
-
-        return if (!TextUtils.isEmpty(imageUrl))
-            rememberAsyncImagePainter(
-                model = ImageRequest.Builder(LocalContext.current)
-                    .data(imageUrl)
-                    .error(errorDrawableResId)
-                    .placeholder(placeDrawableResId)
-                    .build(),
-                imageLoader =imageLoader
-            ) else
-            rememberAsyncImagePainter(
+        if (TextUtils.isEmpty(imageUrl)) {
+            return rememberAsyncImagePainter(
                 model = ImageRequest.Builder(LocalContext.current)
                     .data(placeDrawableResId)
                     .build(),
-                imageLoader =imageLoader
+                imageLoader = imageLoader
             )
+        }
+        val builder = ImageRequest.Builder(LocalContext.current)
+            .data(imageUrl)
+            .error(errorDrawableResId)
+            .placeholder(placeDrawableResId)
+        if (targetWidth != 0 && targetHeight != 0) {
+            builder.size(targetWidth, targetHeight)
+        }
+        return rememberAsyncImagePainter(
+            model = builder
+                .build(),
+            imageLoader = imageLoader,
+            filterQuality = filterQuality
+        )
 
     }
 
